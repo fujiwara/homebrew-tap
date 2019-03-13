8e5b0bbb298da8e0c168fc3ff9300851b863d874
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2019-03-13 23:20:51 +0900

    use zip

--- kinesis-tailf.rb
+++ kinesis-tailf.rb
@@ -1,8 +1,8 @@
 class KinesisTailf < Formula
   version '0.1.1'
   homepage 'https://github.com/fujiwara/kinesis-tailf'
-  url "https://github.com/fujiwara/kinesis-tailf/releases/download/v0.1.1/kinesis-tailf-v0.1.1-darwin-amd64"
-  sha256 'a97f91dc4d99c4be1667b1af803784cb4ce28e00073e5a5bf6e1347e03413acd'
+  url "https://github.com/fujiwara/kinesis-tailf/releases/download/v0.1.1/kinesis-tailf-v0.1.1-darwin-amd64.zip"
+  sha256 'fcb7e165fbe1c1b78d855b3dfb162ac168468165ec0e16c0ae65351d6d1b5290'
   head 'https://github.com/fujiwara/kinesis-tailf.git'
 
   def install
