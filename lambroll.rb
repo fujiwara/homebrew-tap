b67d2fbe7ce876a999b3f7027b323fb9a32c9e14
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2019-11-14 12:33:16 +0900

    lambroll v0.3.0

--- lambroll.rb
+++ lambroll.rb
@@ -1,8 +1,8 @@
 class Lambroll < Formula
-  version '0.2.2'
+  version '0.3.0'
   homepage 'https://github.com/fujiwara/lambroll'
-  url "https://github.com/fujiwara/lambroll/releases/download/v0.2.2/lambroll_v0.2.2_darwin_amd64.zip"
-  sha256 '935b4b5e0e54181df5227bed7f2c255700557e874ba06a204bd56b5012ed5cfe'
+  url "https://github.com/fujiwara/lambroll/releases/download/v0.3.0/lambroll_v0.3.0_darwin_amd64.zip"
+  sha256 '87012bbfd4b6ab0b5c76acdd59f4e9163dd01d75e69ea143bc883493b7b22784'
   head 'https://github.com/fujiwara/lambroll.git'
 
   head do
