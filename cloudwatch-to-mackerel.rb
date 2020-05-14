5746529634b5cf8a99b8df58d4a75ea48903d4ba
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-05-14 09:35:33 +0900

    cloudwatch-to-mackerel v0.0.4

--- cloudwatch-to-mackerel.rb
+++ cloudwatch-to-mackerel.rb
@@ -1,8 +1,8 @@
 class CloudwatchToMackerel < Formula
-  version '0.0.1'
+  version '0.0.4'
   homepage 'https://github.com/fujiwara/cloudwatch-to-mackerel'
-  url "https://github.com/fujiwara/cloudwatch-to-mackerel/releases/download/v0.0.1/cloudwatch-to-mackerel_v0.0.1_darwin_amd64.zip"
-  sha256 'a425ffadda63c9ac79b759d75c1478e5ca22c276c7cb0a0fbdb9c660a589c096'
+  url "https://github.com/fujiwara/cloudwatch-to-mackerel/releases/download/v0.0.4/cloudwatch-to-mackerel_v0.0.4_darwin_amd64.zip"
+  sha256 '6a1099a0ccfad14ac404f2eec6547b126dd7131e5fa70618178481f59170b877'
   head 'https://github.com/fujiwara/cloudwatch-to-mackerel.git'
 
   def install
