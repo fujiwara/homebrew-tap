5795be11543b1c079a551763104ee321542b307c
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-05-29 23:45:29 +0900

    tfstate-lookup v0.0.8

--- tfstate-lookup.rb
+++ tfstate-lookup.rb
@@ -1,8 +1,8 @@
 class TfstateLookup < Formula
-  version '0.0.4'
+  version '0.0.8'
   homepage 'https://github.com/fujiwara/tfstate-lookup'
-  url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.0.4/tfstate-lookup_0.0.4_darwin_amd64.tar.gz"
-  sha256 'e22d25d0360505c49923ec828744e8fc0d99b8576bc43787fd3f93d109d01758'
+  url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.0.8/tfstate-lookup_0.0.8_darwin_amd64.tar.gz"
+  sha256 'ca8a8a56b1aaa63ebd9f8c38d387f58f73ed0dba0b2d2e58e8940e90c6636e11'
   head 'https://github.com/fujiwara/tfstate-lookup.git'
 
   head do
