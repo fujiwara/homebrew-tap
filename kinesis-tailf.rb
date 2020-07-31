f2599e5509ffcaa4d81414b941710181f5156d72
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-07-31 15:12:27 +0900

    kinesis-tailf v0.2.1

--- kinesis-tailf.rb
+++ kinesis-tailf.rb
@@ -1,12 +1,18 @@
 class KinesisTailf < Formula
-  version '0.1.1'
+  version '0.2.1'
   homepage 'https://github.com/fujiwara/kinesis-tailf'
-  url "https://github.com/fujiwara/kinesis-tailf/releases/download/v0.1.1/kinesis-tailf-v0.1.1-darwin-amd64.zip"
-  sha256 'fcb7e165fbe1c1b78d855b3dfb162ac168468165ec0e16c0ae65351d6d1b5290'
+  url "https://github.com/fujiwara/kinesis-tailf/releases/download/v0.2.1/kinesis-tailf_0.2.1_darwin_amd64.tar.gz"
+  sha256 '7b84c53c34873593f83910e50704b3bac637ebec0676432cb5d11afb1bdff80f'
   head 'https://github.com/fujiwara/kinesis-tailf.git'
 
+  head do
+    depends_on 'go' => :build
+  end
+
   def install
-    system 'mv', 'kinesis-tailf-v0.1.1-darwin-amd64', 'kinesis-tailf'
+    if build.head?
+      system 'make', 'build'
+    end
     bin.install 'kinesis-tailf'
   end
 end
