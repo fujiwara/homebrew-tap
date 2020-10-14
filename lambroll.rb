d07956e345239300d86ceffb570b95c51475cc12
fujiwara <fujiwara.shunichiro@gmail.com>
2020-10-14 18:41:28 +0000

    update to newer version

--- lambroll.rb
+++ lambroll.rb
@@ -1,13 +1,13 @@
 class Lambroll < Formula
-  version '0.9.2'
+  version '0.9.3'
   homepage 'https://github.com/fujiwara/lambroll'
   if OS.mac?
-    url "https://github.com/fujiwara/lambroll/releases/download/v0.9.2/lambroll_v0.9.2_darwin_amd64.zip"
-    sha256 '9939de8990599cf0c05155fabc9b9ffd737f90c8c889bf11e48f35e28d2b4832'
+    url "https://github.com/fujiwara/lambroll/releases/download/v0.9.3/lambroll_v0.9.3_darwin_amd64.zip"
+    sha256 'f7f135e59c4c38e41ee8d80ce8c4fb5a387230d4382e59512c64806ad0cac41b'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/lambroll/releases/download/v0.9.2/lambroll_v0.9.2_linux_amd64.tar.gz"
-    sha256 '9c98ba6a0bb2eedfbcab83fedfd2e5c7a074ec1d2aa4ae618ef99753aff0df49'
+    url "https://github.com/fujiwara/lambroll/releases/download/v0.9.3/lambroll_v0.9.3_linux_amd64.tar.gz"
+    sha256 '087d00a9113a5b2111aa8539136b701071138423b4059c2ddc6b55568a506c40'
   end
   head 'https://github.com/fujiwara/lambroll.git'
 
--- tfstate-lookup.rb
+++ tfstate-lookup.rb
@@ -1,13 +1,13 @@
 class TfstateLookup < Formula
-  version '0.0.13'
+  version '0.0.14'
   homepage 'https://github.com/fujiwara/tfstate-lookup'
   if OS.mac?
-    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.0.13/tfstate-lookup_0.0.13_darwin_amd64.tar.gz"
-    sha256 'ab345238f22087ed7cd42723b122a7d80a10fa6c00753a0c9678d6dee401e825'
+    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.0.14/tfstate-lookup_0.0.14_darwin_amd64.tar.gz"
+    sha256 'd8bba721da10bdd583dcf5940b3a68f3a65029a63caafa59468ecd6170f3c4a0'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.0.13/tfstate-lookup_0.0.13_linux_amd64.tar.gz"
-    sha256 '319ff26ea76da8a85963ccf55ec1d72251a7286d28aa9424ac35fbe3d8bad76a'
+    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.0.14/tfstate-lookup_0.0.14_linux_amd64.tar.gz"
+    sha256 '9350f87bddca49112637f74463cdbea49070c1786aaab988e3084329b61c42af'
   end
   head 'https://github.com/fujiwara/tfstate-lookup.git'
 
