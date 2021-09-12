bd7c4a8099813980d67d5b9a163c5e3189922712
fujiwara <fujiwara.shunichiro@gmail.com>
2021-09-12 00:54:31 +0000

    update to newer version

--- lambroll.rb
+++ lambroll.rb
@@ -1,13 +1,13 @@
 class Lambroll < Formula
-  version '0.11.7'
+  version '0.11.8'
   homepage 'https://github.com/fujiwara/lambroll'
   if OS.mac?
-    url "https://github.com/fujiwara/lambroll/releases/download/v0.11.7/lambroll_v0.11.7_darwin_amd64.zip"
-    sha256 '3eb607f5f73be81aee930277665c2fb6d629edda5a88070adc1b9ab18f960887'
+    url "https://github.com/fujiwara/lambroll/releases/download/v0.11.8/lambroll_v0.11.8_darwin_amd64.zip"
+    sha256 '2bf536faa1fe5465fbfda7865351790625ad0c084b0df511a282786eb4963732'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/lambroll/releases/download/v0.11.7/lambroll_v0.11.7_linux_amd64.tar.gz"
-    sha256 'c886f48985484dadc0f045cdadc2d5b62802ea4c3a266484e4cadb79e534d1aa'
+    url "https://github.com/fujiwara/lambroll/releases/download/v0.11.8/lambroll_v0.11.8_linux_amd64.tar.gz"
+    sha256 'c32e6018bf30c633f657fd226d015f5e372641def6335e2cb7b9365a758dc298'
   end
   head 'https://github.com/fujiwara/lambroll.git'
 
