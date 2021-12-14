532ff3334b2576b2c0d90e7140dd21b2d4da5663
fujiwara <fujiwara.shunichiro@gmail.com>
2021-12-14 05:38:40 +0000

    update to newer version

--- tfstate-lookup.rb
+++ tfstate-lookup.rb
@@ -1,13 +1,13 @@
 class TfstateLookup < Formula
-  version '0.4.0'
+  version '0.4.2'
   homepage 'https://github.com/fujiwara/tfstate-lookup'
   if OS.mac?
-    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.4.0/tfstate-lookup_0.4.0_darwin_amd64.tar.gz"
-    sha256 'd6433cf69d514a24f4394ccf4b39d2441b42179cdf5ea7ad792b8ddc41c9cef4'
+    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.4.2/tfstate-lookup_0.4.2_darwin_amd64.tar.gz"
+    sha256 '45b8ab76896a68c406b0bf1db7eb0b24e16986b676258d614986eb36b5818954'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.4.0/tfstate-lookup_0.4.0_linux_amd64.tar.gz"
-    sha256 '31b970e6182daf93907fe0bb7d9c8c652351d9f3f9244b81efcd66e5ce500c65'
+    url "https://github.com/fujiwara/tfstate-lookup/releases/download/v0.4.2/tfstate-lookup_0.4.2_linux_amd64.tar.gz"
+    sha256 'b5f642f4bef730120afc410086057041e9fc831f6bfc0d5ecaa000d3cfbcb487'
   end
   head 'https://github.com/fujiwara/tfstate-lookup.git'
 
