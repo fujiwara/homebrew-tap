2a4bb4a327b87c52a04454523dff5fdca9a6b71f
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-03-12 01:04:05 +0900

    rin v1.2.0

--- rin.rb
+++ rin.rb
@@ -1,13 +1,13 @@
 class Rin < Formula
-  version '1.1.4'
+  version '1.2.0'
   homepage 'https://github.com/fujiwara/rin'
   if OS.mac?
-    url "https://github.com/fujiwara/Rin/releases/download/v1.1.4/Rin_1.1.4_darwin_amd64.tar.gz"
-    sha256 '8d45972aed6e8e83c76ea394ff3d49992fed8bdf4beac5730cbda8fd476018a5'
+    url "https://github.com/fujiwara/Rin/releases/download/v1.2.0/Rin_1.2.0_darwin_amd64.tar.gz"
+    sha256 'fb6186659721edd609f1e1fb396f2c760f643cba15db106a913a9b0ab3bf51d5'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/Rin/releases/download/v1.1.4/Rin_1.1.4_linux_amd64.tar.gz"
-    sha256 '1173448b185864a6c42f6a74611b9805a6c37be6b2c6e590924ed0f4ed6ad811'
+    url "https://github.com/fujiwara/Rin/releases/download/v1.2.0/Rin_1.2.0_linux_amd64.tar.gz"
+    sha256 '071abfdc6c4298e2012471dc6b5755f9df986e09fc328f605c9305efc1507757'
   end
   head 'https://github.com/fujiwara/rin.git'
 
