effa776dd16441057223a6ac790bd1ca0f81f2d7
fujiwara <fujiwara.shunichiro@gmail.com>
2022-08-10 09:35:35 +0000

    update to newer version

--- ecsta.rb
+++ ecsta.rb
@@ -1,13 +1,13 @@
 class Ecsta < Formula
-  version '0.0.3'
+  version '0.0.4'
   homepage 'https://github.com/fujiwara/ecsta'
   if OS.mac?
-    url "https://github.com/fujiwara/ecsta/releases/download/v0.0.3/ecsta_0.0.3_darwin_amd64.tar.gz"
-    sha256 'e484a70c3ce28f7fb12ca03f8dfa12cfbe0f8ab9e2e4ab0642eb474fd2c7fc42'
+    url "https://github.com/fujiwara/ecsta/releases/download/v0.0.4/ecsta_0.0.4_darwin_amd64.tar.gz"
+    sha256 '4a99f3a665c584fea976ad859a62c2abc40f6d6d271e26bf7238c0667b76095f'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/ecsta/releases/download/v0.0.3/ecsta_0.0.3_linux_amd64.tar.gz"
-    sha256 '4908fece7a6c3c8807dfc9e5a060cca6499e1b07f839efc066b4d7d0b30434bc'
+    url "https://github.com/fujiwara/ecsta/releases/download/v0.0.4/ecsta_0.0.4_linux_amd64.tar.gz"
+    sha256 '8d12a8f4d12a48f82f5a52116817bd5ab6255f1211f2b075cc6fb3834dd704ab'
   end
   head 'https://github.com/fujiwara/ecsta.git'
 
