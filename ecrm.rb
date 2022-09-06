272ecc9de7947fedec7acba6df4ae9c64759af42
fujiwara <fujiwara.shunichiro@gmail.com>
2022-09-06 03:29:27 +0000

    update to newer version

--- ecrm.rb
+++ ecrm.rb
@@ -1,13 +1,13 @@
 class Ecrm < Formula
-  version '0.3.2'
+  version '0.3.3'
   homepage 'https://github.com/fujiwara/ecrm'
   if OS.mac?
-    url "https://github.com/fujiwara/ecrm/releases/download/v0.3.2/ecrm_0.3.2_darwin_amd64.tar.gz"
-    sha256 'cd03f46d7f0da1cc85a479ee02f33dfd2f0c3f584e20d40076f7c37f6cdcc857'
+    url "https://github.com/fujiwara/ecrm/releases/download/v0.3.3/ecrm_0.3.3_darwin_amd64.tar.gz"
+    sha256 '2d0c77ae23a4e0a09e8821c61489028c25e7b68fd8765a68457aa02780db929e'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/ecrm/releases/download/v0.3.2/ecrm_0.3.2_linux_amd64.tar.gz"
-    sha256 '99279173fd2fa0afb071f490a97fbc67c51db68e8a6f236e62de13e7a1104e71'
+    url "https://github.com/fujiwara/ecrm/releases/download/v0.3.3/ecrm_0.3.3_linux_amd64.tar.gz"
+    sha256 'e1a831f43a7110475151c36bd03364001d4b1f3ad12138c3b36c42e2f020dd8f'
   end
   head 'https://github.com/fujiwara/ecrm.git'
 
