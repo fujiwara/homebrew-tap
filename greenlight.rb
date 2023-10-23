4a07e33be37ed58d348a1d5f6c25881985d59e12
fujiwara <fujiwara.shunichiro@gmail.com>
2023-10-23 09:33:04 +0000

    update to newer version

--- greenlight.rb
+++ greenlight.rb
@@ -1,27 +1,27 @@
 class Greenlight < Formula
   desc 'greenlight is a graceful health check agent.'
-  version '0.0.4'
+  version '0.0.5'
   homepage 'https://github.com/fujiwara/greenlight'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.4/greenlight_0.0.4_darwin_arm64.tar.gz'
-      sha256 'c527c592b77425eb194a548d032a4cb8a130b71e912505aad491e87ea06105a1'
+      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.5/greenlight_0.0.5_darwin_arm64.tar.gz'
+      sha256 'af8926eec03995722edd12f150cba19fa3457cc3a8d95a8daae28438bfcc11d9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.4/greenlight_0.0.4_darwin_amd64.tar.gz'
-      sha256 '700423690b9fa5b5670d5b71662db157aef2588f761b679534bbe434d15b2153'
+      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.5/greenlight_0.0.5_darwin_amd64.tar.gz'
+      sha256 '07e8e0f89e463c793f46539ec479a4fe41ec79244852b45e2b9530c3ecfda985'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.4/greenlight_0.0.4_linux_arm64.tar.gz'
-      sha256 'b5ed882c185e9ea4b3aadee256e2be4c86874d3a4a2dea72dede54dadf883a0c'
+      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.5/greenlight_0.0.5_linux_arm64.tar.gz'
+      sha256 '01f91e25174df4ad72170dcce9724a06704bb4ece82f2c1fdf4111e0988d9d68'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.4/greenlight_0.0.4_linux_amd64.tar.gz'
-      sha256 'f1baa2bafe1defb3f0174e6a59b446cae88852f8339e7bf9c5fc6e112c2b0ccb'
+      url 'https://github.com/fujiwara/greenlight/releases/download/v0.0.5/greenlight_0.0.5_linux_amd64.tar.gz'
+      sha256 '835e23a5b717eac03442aecfa31dc0f70718a69f7c4e668fd3c18b0081d0e615'
     end
   end
 
