a0fe925b0a4bdc6bf043c6b62e11f411698da284
fujiwara <fujiwara.shunichiro@gmail.com>
2024-01-02 09:33:18 +0000

    update to newer version

--- cfft.rb
+++ cfft.rb
@@ -1,27 +1,27 @@
 class Cfft < Formula
   desc 'cfft is a testing tool for CloudFront Functions.'
-  version '0.0.8'
+  version '0.1.0'
   homepage 'https://github.com/fujiwara/cfft'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/cfft/releases/download/v0.0.8/cfft_0.0.8_darwin_arm64.tar.gz'
-      sha256 '3927fa0d20390d6ea6d22c98286ca917431b899ab19d32eca584e8b364ba1883'
+      url 'https://github.com/fujiwara/cfft/releases/download/v0.1.0/cfft_0.1.0_darwin_arm64.tar.gz'
+      sha256 '7939fa2871bd73548a748f5756954a323b3858859ca5dcc2b5e7e0db1f4756f0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/cfft/releases/download/v0.0.8/cfft_0.0.8_darwin_amd64.tar.gz'
-      sha256 'ba060ff89483fa28a84da0cd7d31da2765b93e48dcff258244b28ae76b25a87f'
+      url 'https://github.com/fujiwara/cfft/releases/download/v0.1.0/cfft_0.1.0_darwin_amd64.tar.gz'
+      sha256 'ba9a73df9bd376944cdfc22c7877fb11bc1122379d577656f392fc1b63632902'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/cfft/releases/download/v0.0.8/cfft_0.0.8_linux_arm64.tar.gz'
-      sha256 '835ef96771db821dedda56f584e81eba8c086b63e1d1912c05131f4728af958b'
+      url 'https://github.com/fujiwara/cfft/releases/download/v0.1.0/cfft_0.1.0_linux_arm64.tar.gz'
+      sha256 'e921d8dae4ca065670d3f5140e4acc4aee7197afa20d9a2c3008b73ab013f9d1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/cfft/releases/download/v0.0.8/cfft_0.0.8_linux_amd64.tar.gz'
-      sha256 '58179dfb64c27cc6d4ce8245641413c2dff9b3644b3e4bea30eb0e3bc893d487'
+      url 'https://github.com/fujiwara/cfft/releases/download/v0.1.0/cfft_0.1.0_linux_amd64.tar.gz'
+      sha256 '98111a728ab16a1071c4c44ea95661276a43ca07a2f1f4b978e8224e0e48e8ed'
     end
   end
 
--- tfstate-lookup.rb
+++ tfstate-lookup.rb
@@ -1,27 +1,27 @@
 class TfstateLookup < Formula
   desc 'Lookup resource attributes in tfstate.'
-  version '1.1.5'
+  version '1.1.6'
   homepage 'https://github.com/fujiwara/tfstate-lookup'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.5/tfstate-lookup_1.1.5_darwin_arm64.tar.gz'
-      sha256 'c7bdd9bf8cd66a4ec726f9ffcb7243f818f5df9af40b32e2c1a2e895599f0a5d'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.6/tfstate-lookup_1.1.6_darwin_arm64.tar.gz'
+      sha256 'e1d7fe3562bd8b894438e22d43ed2e846c5ff4405f7e4e1d9277c33dc30bd36a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.5/tfstate-lookup_1.1.5_darwin_amd64.tar.gz'
-      sha256 'f26e0cfc30d0a1445635f61ebb10bf34782bf8977accd8a4d246b239523ef7ff'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.6/tfstate-lookup_1.1.6_darwin_amd64.tar.gz'
+      sha256 '1e27a567de6b24dc9291b003078cd209efbf5efdff993899a65fc7f7f0f256fa'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.5/tfstate-lookup_1.1.5_linux_arm64.tar.gz'
-      sha256 '46898ea6a58bf5b67de76058979d93dd96d75a9379e5b325682a91a6c774825e'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.6/tfstate-lookup_1.1.6_linux_arm64.tar.gz'
+      sha256 '3e9588315c49664d1689e79f2368ea8ec34672b1496af0643881b6173c8069d3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.5/tfstate-lookup_1.1.5_linux_amd64.tar.gz'
-      sha256 '5f83389f6461aa4699c85f7d404efc135a29664139d2c7d30db8d2f530afdd8d'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.6/tfstate-lookup_1.1.6_linux_amd64.tar.gz'
+      sha256 'bcaae9faf897ac168a232246ef10210316a1499e127a1d520f6b68f9a330ba33'
     end
   end
 
