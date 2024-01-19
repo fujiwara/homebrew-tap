b5cfc3def1e22940e1d071f1e83c7a66fca9623e
fujiwara <fujiwara.shunichiro@gmail.com>
2024-01-19 18:07:49 +0000

    update to newer version

--- maprobe.rb
+++ maprobe.rb
@@ -1,13 +1,13 @@
 class Maprobe < Formula
-  version '0.7.0'
+  version '0.7.1'
   homepage 'https://github.com/fujiwara/maprobe'
   if OS.mac?
-    url "https://github.com/fujiwara/maprobe/releases/download/v0.7.0/maprobe_v0.7.0_darwin_amd64.zip"
-    sha256 '58b5cd689c976dc1ac946eacc5d86fbd951d6587a98f91745d297d977203e25c'
+    url "https://github.com/fujiwara/maprobe/releases/download/v0.7.1/maprobe_v0.7.1_darwin_amd64.zip"
+    sha256 '043551463bdc5174a1b46c591d5e2ada5d6bcf1d6bc2ce8b03aea0c28cd3572d'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/maprobe/releases/download/v0.7.0/maprobe_v0.7.0_linux_amd64.zip"
-    sha256 '1c45393e3384c0528c5a885794c64bdaa32ba63172bd0f200a2d30c040662eb0'
+    url "https://github.com/fujiwara/maprobe/releases/download/v0.7.1/maprobe_v0.7.1_linux_amd64.zip"
+    sha256 'c0421e27e935bf2e735797ea789517c8f6e437eb2a6d2c3e382391c188cb681b'
   end
   head 'https://github.com/fujiwara/maprobe.git'
 
