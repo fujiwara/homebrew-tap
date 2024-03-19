aa7eb4b6c0a4c2c6ee274b2bf9c9719c7a6a01cc
fujiwara <fujiwara.shunichiro@gmail.com>
2024-03-19 17:08:13 +0000

    update to newer version

--- tfstate-lookup.rb
+++ tfstate-lookup.rb
@@ -1,27 +1,27 @@
 class TfstateLookup < Formula
   desc 'Lookup resource attributes in tfstate.'
-  version '1.1.7'
+  version '1.2.0'
   homepage 'https://github.com/fujiwara/tfstate-lookup'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.7/tfstate-lookup_1.1.7_darwin_arm64.tar.gz'
-      sha256 'f6ef5a8fd10ea8d71dbf947dee25f7e8a78fc4efc1c37694f38b647cf4d6fc50'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.2.0/tfstate-lookup_1.2.0_darwin_arm64.tar.gz'
+      sha256 '22e5f3db47f78a9d4068bd02c5139bee54fb634e6b0ec3dc746fc3929fc22edc'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.7/tfstate-lookup_1.1.7_darwin_amd64.tar.gz'
-      sha256 '968fda2bd250283f333e4670505764d5826499bf23a394cca41ef0b453067c4b'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.2.0/tfstate-lookup_1.2.0_darwin_amd64.tar.gz'
+      sha256 '7450bbb8581316a59a411b6eb49d4c272d66b4362022c94ccaa5671b119823f0'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.7/tfstate-lookup_1.1.7_linux_arm64.tar.gz'
-      sha256 '8ffdf2dcdb597157fc9bb4d202a697af746ebb77b048ec123d301e6b5be0ae4c'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.2.0/tfstate-lookup_1.2.0_linux_arm64.tar.gz'
+      sha256 'fbfd7db814cd46b6cb696bc21769ad8a20fa43ec9977ecf208d30818d611a65c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.1.7/tfstate-lookup_1.1.7_linux_amd64.tar.gz'
-      sha256 'e218ff21bf2aa4bb2ebd181b762c8d4cb93c1683b44c3153c8b2376118a7533c'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.2.0/tfstate-lookup_1.2.0_linux_amd64.tar.gz'
+      sha256 'a9ff5392e965cbba79c672d475312f857183e1eae20b5634318cfe39bac2f4e9'
     end
   end
 
