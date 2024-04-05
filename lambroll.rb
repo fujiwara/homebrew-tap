02f9786c010948e0b93d0af6bc56d04fab9fd40d
fujiwara <fujiwara.shunichiro@gmail.com>
2024-04-05 16:41:30 +0000

    update to newer version

--- lambroll.rb
+++ lambroll.rb
@@ -1,27 +1,27 @@
 class Lambroll < Formula
   desc 'lambroll is a minimal deployment tool for AWS Lambda.'
-  version '1.0.2'
+  version '1.0.3'
   homepage 'https://github.com/fujiwara/lambroll'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.2/lambroll_v1.0.2_darwin_arm64.tar.gz'
-      sha256 '1b81ff13451a049ea5a15cf0e5fe47c03a07d2c874fa42cbfe2cbfbe30406850'
+      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.3/lambroll_v1.0.3_darwin_arm64.tar.gz'
+      sha256 '962557932180128a36e678f42609d66724161e09b58a1c4f9e12029367180692'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.2/lambroll_v1.0.2_darwin_amd64.tar.gz'
-      sha256 '017cdcf16345d1683f935e7203a5a679a1740c7acbed30af28a1a8d0d8600d6a'
+      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.3/lambroll_v1.0.3_darwin_amd64.tar.gz'
+      sha256 'f07d8f79fa3db8c8a15b087f68bc2cc3af3c3546bdf65fd26de4208e4e1236e5'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.2/lambroll_v1.0.2_linux_arm64.tar.gz'
-      sha256 'c820d332a03ee31fc4368b55bbe03e538561f975c2943d190daaa91d7f5984ed'
+      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.3/lambroll_v1.0.3_linux_arm64.tar.gz'
+      sha256 '67a8314230d0b6f4719428e111fa1b436b506a0f495a8fb307db3113bac66cf4'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.2/lambroll_v1.0.2_linux_amd64.tar.gz'
-      sha256 '84c59a9d1fa345763df2b1cf0ff6fc87ba0db90b8de3cc11645c612c26506638'
+      url 'https://github.com/fujiwara/lambroll/releases/download/v1.0.3/lambroll_v1.0.3_linux_amd64.tar.gz'
+      sha256 '110b4f9cb12d3b075913e9072474e61538d9e5fc72e0fef15760e50f4eaf9a13'
     end
   end
 
