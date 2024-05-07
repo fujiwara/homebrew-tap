58bbd71abe10ea52b24f74ad1d8930297535dbc3
fujiwara <fujiwara.shunichiro@gmail.com>
2024-05-07 09:32:42 +0000

    update to newer version

--- kinesis-tailf.rb
+++ kinesis-tailf.rb
@@ -1,27 +1,27 @@
 class KinesisTailf < Formula
   desc 'tail -f command for Amazon Kinesis Stream'
-  version '0.4.0'
+  version '0.5.0'
   homepage 'https://github.com/fujiwara/kinesis-tailf'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.4.0/kinesis-tailf_0.4.0_darwin_arm64.tar.gz'
-      sha256 'b5156bcb969fec08d0bf099deea51109031dba7ab945b677b338d7cbc35c8ba0'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_darwin_arm64.tar.gz'
+      sha256 'b98ccea9d7c2a03a351577a8c94ed829d9e7323d9fb4c005268f67e408ed0717'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.4.0/kinesis-tailf_0.4.0_darwin_amd64.tar.gz'
-      sha256 'bb87a3106c91ae70c8327aee985a9fbb2c2ded5ad735fbe05c617dfbfa9b74e3'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_darwin_amd64.tar.gz'
+      sha256 'bde1fa83bd5a3439c2aaa1e0e6f77bb44d6844b09fe32b30f91b505a92b436de'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.4.0/kinesis-tailf_0.4.0_linux_arm64.tar.gz'
-      sha256 '36b9f6304496f654326da1baa8dbac90f934bb90f2a722e8927b3e49f0749303'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_linux_arm64.tar.gz'
+      sha256 '96ecf927be0f784b83d51726cf0c9b3db363df5b62537b53d6cc9a5a76a9a48c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.4.0/kinesis-tailf_0.4.0_linux_amd64.tar.gz'
-      sha256 'af7917ba7b8301591a7c26fa2c8e340899c8e7a358bb2427dd17fda1e03cfade'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_linux_amd64.tar.gz'
+      sha256 '9c08ebe62f9a4893790140074dc8deac0391d1c935da6895657e0e3457523c3c'
     end
   end
 
