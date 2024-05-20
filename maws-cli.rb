774b7c99fa78685984cda5af0a37bc4e25fe37a1
fujiwara <fujiwara.shunichiro@gmail.com>
2024-05-20 09:15:39 +0000

    update to newer version

--- maws-cli.rb
+++ maws-cli.rb
@@ -1,13 +1,13 @@
 class MawsCli < Formula
-  version '0.0.3'
+  version '0.0.4'
   homepage 'https://github.com/fujiwara/maws-cli'
   if OS.mac?
-    url "https://github.com/fujiwara/maws-cli/releases/download/v0.0.3/maws-cli_0.0.3_darwin_amd64.tar.gz"
-    sha256 '838e629575f5de0ba63dd42911b15af006f7f904b12f401df89a3956da41c02c'
+    url "https://github.com/fujiwara/maws-cli/releases/download/v0.0.4/maws-cli_0.0.4_darwin_amd64.tar.gz"
+    sha256 'f2aa8f6f575b156a014d838a7edaa99124ca0b4d40e7af8af2f015da7c0305d8'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/maws-cli/releases/download/v0.0.3/maws-cli_0.0.3_linux_amd64.tar.gz"
-    sha256 'e127ec1499fed5728f59ad5edf9b1a5ef435191e81c807acef7ee5bb164f4d30'
+    url "https://github.com/fujiwara/maws-cli/releases/download/v0.0.4/maws-cli_0.0.4_linux_amd64.tar.gz"
+    sha256 'a771be53fa0a17713b55e7393a1d3e0edb7ca79c08fc1181042d1c1294a584a5'
   end
   head 'https://github.com/fujiwara/maws-cli.git'
 
