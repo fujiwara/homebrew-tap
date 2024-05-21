6e43ffe227d00b9653510cd9fc975c4d7978cddc
fujiwara <fujiwara.shunichiro@gmail.com>
2024-05-21 09:33:00 +0000

    update to newer version

--- aws-sdk-client-go.rb
+++ aws-sdk-client-go.rb
@@ -1,27 +1,27 @@
 class AwsSdkClientGo < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.0.11'
+  version '0.0.12'
   homepage 'https://github.com/fujiwara/aws-sdk-client-go'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.11/aws-sdk-client-go_0.0.11_darwin_arm64.tar.gz'
-      sha256 'e5c064b2806d36760b66875821f4e44541bc28f69c4de08f60db2d852a660b26'
+      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.12/aws-sdk-client-go_0.0.12_darwin_arm64.tar.gz'
+      sha256 'a347a3114132d1b77e791a733b8747fc3b35e2db055685286b962a683e330eaf'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.11/aws-sdk-client-go_0.0.11_darwin_amd64.tar.gz'
-      sha256 'b5f53a3797316e6ee5311505ccd1c2d0ebb0848fca6711035baf2f51f30d08dc'
+      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.12/aws-sdk-client-go_0.0.12_darwin_amd64.tar.gz'
+      sha256 '3f7c91a5738fe0e68bc612b7d3ae5d2738dc8be5690dfa55380795744bcf1ce0'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.11/aws-sdk-client-go_0.0.11_linux_arm64.tar.gz'
-      sha256 'c95c554054e08feba6d28a971caa36b24f127997fc9a98b60e25555c2b9b3016'
+      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.12/aws-sdk-client-go_0.0.12_linux_arm64.tar.gz'
+      sha256 'c84ef0c5d3c811701b793add6035a53c43392a05a0f8dc0d7e104640ff88225e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.11/aws-sdk-client-go_0.0.11_linux_amd64.tar.gz'
-      sha256 '17eb01de8d2ced1b7f26103463093a470fb611c976122305a33f49eb43e5738b'
+      url 'https://github.com/fujiwara/aws-sdk-client-go/releases/download/v0.0.12/aws-sdk-client-go_0.0.12_linux_amd64.tar.gz'
+      sha256 'dc2106ca10d3c8c9b924b1256984958f3ca30471d0861b2314256e6fd622c6d6'
     end
   end
 
