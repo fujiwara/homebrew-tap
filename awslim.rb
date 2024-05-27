f787350f403af5b09f4e25c4b49ee5289477462c
fujiwara <fujiwara.shunichiro@gmail.com>
2024-05-27 09:33:14 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.1.1'
+  version '0.1.2'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.1/awslim_0.1.1_darwin_arm64.tar.gz'
-      sha256 '9bd9d0cefed140bc685841adf0526ba1a9d21ddc8c87b2b3b703cd23e61538b0'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.2/awslim_0.1.2_darwin_arm64.tar.gz'
+      sha256 'b59cee49f3453110f0dc7fba2e2f37655f6954db5393ac39c32a2b6690bcf312'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.1/awslim_0.1.1_darwin_amd64.tar.gz'
-      sha256 '217f2932a62ea64b4898ab86bf50ea959e3285637705f7ee47de7b1a9d748afe'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.2/awslim_0.1.2_darwin_amd64.tar.gz'
+      sha256 '22bcaaea7b556068caff29fa8c0b3d6eb85da889b7e793b97435a9dfbe432349'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.1/awslim_0.1.1_linux_arm64.tar.gz'
-      sha256 '6f89faf684424bbd002023b4adac4d2f4922d86fc9f39011d0775b16f388bb97'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.2/awslim_0.1.2_linux_arm64.tar.gz'
+      sha256 '17fd3f5a4625847b4eb68ca9a478d4afaf8e51e7c38670f5b0aecd63d0a31b6d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.1/awslim_0.1.1_linux_amd64.tar.gz'
-      sha256 'de4ab5b68104e6bc9f7a4acf7674c5b596885786abb60e6add5bd1135eded88b'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.2/awslim_0.1.2_linux_amd64.tar.gz'
+      sha256 '29b571e6ca7c4a888f7f3bf5cc17f4d184e80b0d9bd969b69320d4f1daec1cdb'
     end
   end
 
