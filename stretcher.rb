14679b14d0c1fac486eb61f652e41e49c029d1a8
fujiwara <fujiwara.shunichiro@gmail.com>
2024-06-04 04:49:12 +0000

    update to newer version

--- stretcher.rb
+++ stretcher.rb
@@ -1,13 +1,13 @@
 class Stretcher < Formula
-  version '1.0.2'
+  version '1.1.0'
   homepage 'https://github.com/fujiwara/stretcher'
   if OS.mac?
-    url "https://github.com/fujiwara/stretcher/releases/download/v1.0.2/stretcher-v1.0.2-darwin-amd64.zip"
-    sha256 'c82ff48a28d060ef1f64c61ccd7e064217526a5930e42f7149e8e598ce8b62bb'
+    url "https://github.com/fujiwara/stretcher/releases/download/v1.1.0/stretcher-v1.1.0-darwin-amd64.zip"
+    sha256 '1820d440ef7dda55b6fdd97f699ec7a8f0aff5e9c782d806e7602f780752de67'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/stretcher/releases/download/v1.0.2/stretcher-v1.0.2-linux-amd64.zip"
-    sha256 'd5fba88f0c23daa31dd16e5fee25665bc90210cead8b3c503a16eeee8d57ff1a'
+    url "https://github.com/fujiwara/stretcher/releases/download/v1.1.0/stretcher-v1.1.0-linux-amd64.zip"
+    sha256 '1506412d1974a6652cb11c601d0566f6063ab39a51bbb0cf0dd687318accd3c0'
   end
   head 'https://github.com/fujiwara/stretcher.git'
 
