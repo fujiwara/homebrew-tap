80447563d65119029935ce845f127b1870760209
fujiwara <fujiwara.shunichiro@gmail.com>
2024-07-03 06:33:09 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.1.3'
+  version '0.2.0'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.3/awslim_0.1.3_darwin_arm64.tar.gz'
-      sha256 '6529dc7bf24ebb5388d452f41f4e90a143c875c0ebab9f74bff97f0544f5ae2d'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.2.0/awslim_0.2.0_darwin_arm64.tar.gz'
+      sha256 '15515e37236d4c3c91576a0220a5a0d8ebe06d98dba07195eed6f53d12ab0cd6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.3/awslim_0.1.3_darwin_amd64.tar.gz'
-      sha256 'a4bdf4535b5d617026158d42cc95c969f2524ebcb2e005affb91b314f9dd203b'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.2.0/awslim_0.2.0_darwin_amd64.tar.gz'
+      sha256 '50970307ae9a7ce866e240fc0c124010c5a7f9d2b7a5844c45aed79800bb8cb4'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.3/awslim_0.1.3_linux_arm64.tar.gz'
-      sha256 '4ffdb825f5e9e4cadeed381dba0574636c093f530472d3a2cb54518139efdbd7'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.2.0/awslim_0.2.0_linux_arm64.tar.gz'
+      sha256 '4791ecae7e49edfa944a8eb7b88d2d06081fbe81c1fd79847d80c2f5107e0784'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.1.3/awslim_0.1.3_linux_amd64.tar.gz'
-      sha256 '3f82938808d5bade2dcf748d290adc6bf9810fa642c642a0b60c0c9b5416a062'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.2.0/awslim_0.2.0_linux_amd64.tar.gz'
+      sha256 '2a7e13fd236acb986bf163375cb8366701f9de1fa0e46f4437ffd5c6c720b25b'
     end
   end
 
