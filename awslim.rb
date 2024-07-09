06151abbca47f2b55b40a615e6a1720202027a00
fujiwara <fujiwara.shunichiro@gmail.com>
2024-07-09 09:33:15 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.3.0'
+  version '0.3.1'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.0/awslim_0.3.0_darwin_arm64.tar.gz'
-      sha256 'de215cd4cd7ca4b83060aa3aed7bf7cd99b3874eb93dd214ab21f148671f947d'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_darwin_arm64.tar.gz'
+      sha256 'e2248061c7bfd2d80db38865c13087a762fcfac3ea87790c3baa1f07f1a1ee55'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.0/awslim_0.3.0_darwin_amd64.tar.gz'
-      sha256 '49a333b4d49a0b433fb8a2281e5fa30e40efc3c7f3728958694a1575d8d96bab'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_darwin_amd64.tar.gz'
+      sha256 '3c655e0148acf18c1aed5edc6bac4acc8ce956757ac6622beb29799d815bbb52'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.0/awslim_0.3.0_linux_arm64.tar.gz'
-      sha256 '4b3c6f15b83c1832c87fdc5ca204f7f8916c57a25c28d7ed60ec5b6922e41182'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_linux_arm64.tar.gz'
+      sha256 '8f9f0eb961e4c3c9276d96e25a632d61fc12d2e18597c14f58b787d5f2232c0c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.0/awslim_0.3.0_linux_amd64.tar.gz'
-      sha256 'b82c951d7f0fe43acc89928361b48bfa4faa949cd08e0b3bf7dc946db839cdd3'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_linux_amd64.tar.gz'
+      sha256 'ee7c97da236fb01a84bb7ff9744c3ebb1e65b64dfb4a07a6faadea0b149546cf'
     end
   end
 
