36782bf55ce020546be242033180f4129b830970
fujiwara <fujiwara.shunichiro@gmail.com>
2024-08-02 09:36:43 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.3.1'
+  version '0.3.2'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_darwin_arm64.tar.gz'
-      sha256 'e2248061c7bfd2d80db38865c13087a762fcfac3ea87790c3baa1f07f1a1ee55'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.2/awslim_0.3.2_darwin_arm64.tar.gz'
+      sha256 'bc83908fc22813bc18d72cd8aac01abd144f9317663d8f4a318389f5a3a47d29'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_darwin_amd64.tar.gz'
-      sha256 '3c655e0148acf18c1aed5edc6bac4acc8ce956757ac6622beb29799d815bbb52'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.2/awslim_0.3.2_darwin_amd64.tar.gz'
+      sha256 '92a344b1659e3a2d976063c69776b0a59c411a473750fbfda83df2c3868b970b'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_linux_arm64.tar.gz'
-      sha256 '8f9f0eb961e4c3c9276d96e25a632d61fc12d2e18597c14f58b787d5f2232c0c'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.2/awslim_0.3.2_linux_arm64.tar.gz'
+      sha256 '3d66168a108b6524f6f1a2026d6f41b99b270b0be73c595414639563525ea7fd'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.1/awslim_0.3.1_linux_amd64.tar.gz'
-      sha256 'ee7c97da236fb01a84bb7ff9744c3ebb1e65b64dfb4a07a6faadea0b149546cf'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.2/awslim_0.3.2_linux_amd64.tar.gz'
+      sha256 'f3143fc185f3561d934a42e6ba06139ce62bbb7181d766ecc10db797f2fb355c'
     end
   end
 
