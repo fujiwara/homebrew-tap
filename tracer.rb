fec16a4ece7b62912601341e4b7a91272e51127b
fujiwara <fujiwara.shunichiro@gmail.com>
2024-08-18 09:32:35 +0000

    update to newer version

--- tracer.rb
+++ tracer.rb
@@ -1,27 +1,27 @@
 class Tracer < Formula
   desc 'ECS task event/log tracer CLI'
-  version '1.0.2'
+  version '1.1.0'
   homepage 'https://github.com/fujiwara/tracer'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.0.2/tracer_1.0.2_darwin_arm64.tar.gz'
-      sha256 '6aa52a95eaf176c6f4a11e0be0567ecaeec67ac8a02b7098e43427296248ad35'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.0/tracer_1.1.0_darwin_arm64.tar.gz'
+      sha256 'a355d53c60fb362231308b9a532d4ea016359a7f8f7b29a1223d44596f69406f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.0.2/tracer_1.0.2_darwin_amd64.tar.gz'
-      sha256 '0fe8866c2c469153d59218af052c0726d3ca20fac70095c26d10d1957bc0ade6'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.0/tracer_1.1.0_darwin_amd64.tar.gz'
+      sha256 'c517f57938658d4ce6cac7e363cf77a702a2fcb7cd67d7ac4394d52a865b4909'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.0.2/tracer_1.0.2_linux_arm64.tar.gz'
-      sha256 '26e04a1bf5f21c4ba78eccef076dbcbd467b04576a485de811445594821e5e99'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.0/tracer_1.1.0_linux_arm64.tar.gz'
+      sha256 '35ba554fb53aa09c86f427c6d5185e42eb75fb8452df8d3f2d4b91963800e008'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.0.2/tracer_1.0.2_linux_amd64.tar.gz'
-      sha256 '89402cb97d358e1d5d3e3cd0ad9ed68d634bb04e15f57747e0f42061a0a9893f'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.0/tracer_1.1.0_linux_amd64.tar.gz'
+      sha256 '9a4362b11e107edcb041fc792387a4e63076bc5dad59bbe87aabaee420bb830a'
     end
   end
 
