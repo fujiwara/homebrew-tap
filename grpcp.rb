752ed302df00f65ea60b11d3e204d2fb303022a5
fujiwara <fujiwara.shunichiro@gmail.com>
2024-08-21 09:06:07 +0000

    update to newer version

--- grpcp.rb
+++ grpcp.rb
@@ -1,27 +1,27 @@
 class Grpcp < Formula
   desc 'gRPC stream file transfer server/client'
-  version '0.0.5'
+  version '0.1.0'
   homepage 'https://github.com/fujiwara/grpcp'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/grpcp/releases/download/v0.0.5/grpcp_0.0.5_darwin_arm64.tar.gz'
-      sha256 'ea6cc536cca515dd2b3b569938db8d52349336c9aaf11942be06c8ee27186760'
+      url 'https://github.com/fujiwara/grpcp/releases/download/v0.1.0/grpcp_0.1.0_darwin_arm64.tar.gz'
+      sha256 '9de4f0651d72e28988297590b2bd872cde230075e76f27b35a40ed1ff04b0451'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/grpcp/releases/download/v0.0.5/grpcp_0.0.5_darwin_amd64.tar.gz'
-      sha256 '3368c7deb688a9e8d2556e5ef17d5be3faa25a748b8592c11e8836376e9cce4c'
+      url 'https://github.com/fujiwara/grpcp/releases/download/v0.1.0/grpcp_0.1.0_darwin_amd64.tar.gz'
+      sha256 '1d5bd17bc26dc8f9b7d5849f47b0ff71b91b2187c0051c014994420a6bc1ca11'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/grpcp/releases/download/v0.0.5/grpcp_0.0.5_linux_arm64.tar.gz'
-      sha256 'ce57f8d62fcfd2e59953d27d1a836f8804399103570abcb9d82cadaf3870f4ce'
+      url 'https://github.com/fujiwara/grpcp/releases/download/v0.1.0/grpcp_0.1.0_linux_arm64.tar.gz'
+      sha256 'e0217f5924f48104cbba40386232d5d2a34884924c7fc7b846ab86974fbdd915'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/grpcp/releases/download/v0.0.5/grpcp_0.0.5_linux_amd64.tar.gz'
-      sha256 'fe28ae2d74013982c848a2a4da6045007c9a2e2be743ca5d2015143fe0273972'
+      url 'https://github.com/fujiwara/grpcp/releases/download/v0.1.0/grpcp_0.1.0_linux_amd64.tar.gz'
+      sha256 '708f49d9e5294c407b0a56ad522824ae9452813f4aaf9aeb3311dfcc7785a7da'
     end
   end
 
