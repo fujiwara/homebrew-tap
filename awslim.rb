40403b30d705962e775bd9bade6391efe6702ad1
fujiwara <fujiwara.shunichiro@gmail.com>
2024-08-29 09:33:32 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.3.3'
+  version '0.3.4'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.3/awslim_0.3.3_darwin_arm64.tar.gz'
-      sha256 '4c6929abe395576d4487fd06e49b6a70dc14a0a7dec348a57f48f508378c4eba'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.4/awslim_0.3.4_darwin_arm64.tar.gz'
+      sha256 'cd932619cbb7dd8b417bd60cc5741d43a728d02e8eb72ee1f9015471f30b4e5e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.3/awslim_0.3.3_darwin_amd64.tar.gz'
-      sha256 'cf1cf1340db8d372814442b22939495a32cf1d836ca5e02d6078b5444645c313'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.4/awslim_0.3.4_darwin_amd64.tar.gz'
+      sha256 '27248e77ea557ddec167afdcd8c82991782f2349e2e532de46f94fa0aa6dc01c'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.3/awslim_0.3.3_linux_arm64.tar.gz'
-      sha256 '276799533bf5b617c5d8aca7865b39fcb12d869228a854450c8c465f047ec943'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.4/awslim_0.3.4_linux_arm64.tar.gz'
+      sha256 '81405a24060d7c5de009b459ea2bfbf775ee02b68ecbcf59d0f807bad9b023cd'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.3/awslim_0.3.3_linux_amd64.tar.gz'
-      sha256 'ec2ab9804f7f9d792e0b85bd2427ff89b391696cffa3ba3fd02424e888c3ebd2'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.4/awslim_0.3.4_linux_amd64.tar.gz'
+      sha256 '6daae1c468b6c852b53339ea0a85384b8c245ad9e2b0e07d81b2867ab4f7dc04'
     end
   end
 
