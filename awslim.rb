60f001b4bd7109e46e01c1594ec5659195397e05
fujiwara <fujiwara.shunichiro@gmail.com>
2024-11-14 09:34:21 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.3.6'
+  version '0.3.7'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.6/awslim_0.3.6_darwin_arm64.tar.gz'
-      sha256 'eeed1e78a3bd99ada9769ff30193ea18da64db1cb411a70eccf8fb33d0ef9c92'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.7/awslim_0.3.7_darwin_arm64.tar.gz'
+      sha256 '430f8c0dd8bfdd1e8b719b91b7a42d8e3943e3dc1854898514fa6ddd6aefb89d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.6/awslim_0.3.6_darwin_amd64.tar.gz'
-      sha256 '9816922113ef88b4b5251b2d920d3909fb8602926e9d3f778cbce54f143bdf8e'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.7/awslim_0.3.7_darwin_amd64.tar.gz'
+      sha256 '6fcf75529818a48cecd66febcfb78aa1f41555b178546afdbd565e155c728793'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.6/awslim_0.3.6_linux_arm64.tar.gz'
-      sha256 'f3697fb5a7fdee64af04fb99dde46066bd01f35b79c2000679ffa4a858f5511a'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.7/awslim_0.3.7_linux_arm64.tar.gz'
+      sha256 '8230c8bcc77b7f51fe4567c7eb838b6705dd63ba8afcdebb9ee729ce61dbbd5e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.6/awslim_0.3.6_linux_amd64.tar.gz'
-      sha256 '11e821969a0c2876cbe647b6f469303857fb409cd7faa4b6d1853cb612c3e0ff'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.3.7/awslim_0.3.7_linux_amd64.tar.gz'
+      sha256 'ec5665f1e0b37974d68aa8350ba8f2723c69975f0d392b0571fd14502db71de0'
     end
   end
 
