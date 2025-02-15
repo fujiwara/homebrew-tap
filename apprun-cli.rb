91e0af330c8eb1e60a7883c79ebd1a2b03586323
fujiwara <fujiwara.shunichiro@gmail.com>
2025-02-15 02:28:16 +0000

    update to newer version

--- apprun-cli.rb
+++ apprun-cli.rb
@@ -1,27 +1,27 @@
 class ApprunCli < Formula
   desc 'CLI for sakura AppRun'
-  version '0.0.2'
+  version '0.2.3'
   homepage 'https://github.com/fujiwara/apprun-cli'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.0.2/apprun-cli_0.0.2_darwin_arm64.tar.gz'
-      sha256 'c384dd8be097ca19bf9c1b3b8c81f8a07fe8e7d2eb82d1071ad6b3f9b94c17e6'
+      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.2.3/apprun-cli_0.2.3_darwin_arm64.tar.gz'
+      sha256 '96ca719fd17eea09afc760ccff6a8c3591bbb752221ad22993622ea148faf08a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.0.2/apprun-cli_0.0.2_darwin_amd64.tar.gz'
-      sha256 'c74c8c65dd530e11aa0abcc2800c9e06d46f74be8d5ab5d051fdf251ee52e9c7'
+      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.2.3/apprun-cli_0.2.3_darwin_amd64.tar.gz'
+      sha256 'b766cd74c4bc24f5717338d999f919a8472428e2b6eb61f7d4e7b383a8d633f1'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.0.2/apprun-cli_0.0.2_linux_arm64.tar.gz'
-      sha256 'd5b020ed4153dcb0277103a9fcae21d6c04bb14be1c1268ce6cf5768514c7113'
+      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.2.3/apprun-cli_0.2.3_linux_arm64.tar.gz'
+      sha256 'f8de3fa4b903dfebc1bde812edbd86a45b64df80b7610b26cbe8ab8686ac39cd'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.0.2/apprun-cli_0.0.2_linux_amd64.tar.gz'
-      sha256 '5bdeb955da26accb298ea7fecf2674445abbd84c3f582234944ed0945fbe7c80'
+      url 'https://github.com/fujiwara/apprun-cli/releases/download/v0.2.3/apprun-cli_0.2.3_linux_amd64.tar.gz'
+      sha256 '9e926190bfebe4fd3856d35367521ae6b2fa20929b2edc8d007d5518cdb704de'
     end
   end
 
