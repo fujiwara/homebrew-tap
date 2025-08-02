e8cdc35b584ad26a23a8c9d375fd26cefa94b887
fujiwara <fujiwara.shunichiro@gmail.com>
2025-08-02 07:01:15 +0000

    update to newer version

--- maprobe.rb
+++ maprobe.rb
@@ -1,13 +1,13 @@
 class Maprobe < Formula
-  version '0.7.7'
+  version '0.9.1'
   homepage 'https://github.com/fujiwara/maprobe'
   if OS.mac?
-    url "https://github.com/fujiwara/maprobe/releases/download/v0.7.7/maprobe_v0.7.7_darwin_amd64.zip"
-    sha256 '1cd2369d120216cca430eb8536a9de772e4b63064daed7338b1f8c637a48deac'
+    url "https://github.com/fujiwara/maprobe/releases/download/v0.9.1/maprobe_0.9.1_darwin_amd64.tar.gz"
+    sha256 '2145d55a353874151a2d4ad8b48161a0b7b1cddb2618584638f5192e0004d92a'
   end
   if OS.linux?
-    url "https://github.com/fujiwara/maprobe/releases/download/v0.7.7/maprobe_v0.7.7_linux_amd64.zip"
-    sha256 '08512b768d51e77ef36c953126f8726a77dcbca41bfa43261309eba84e5bbca2'
+    url "https://github.com/fujiwara/maprobe/releases/download/v0.9.1/maprobe_0.9.1_linux_amd64.tar.gz"
+    sha256 '9d0d4a7dd2c9b0f3901ee999e6acccf0e6cde2b026f14530a2ef45d4228a7a80'
   end
   head 'https://github.com/fujiwara/maprobe.git'
 
--- tfstate-lookup.rb
+++ tfstate-lookup.rb
@@ -1,27 +1,27 @@
 class TfstateLookup < Formula
   desc 'Lookup resource attributes in tfstate.'
-  version '1.6.0'
+  version '1.7.0'
   homepage 'https://github.com/fujiwara/tfstate-lookup'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.6.0/tfstate-lookup_1.6.0_darwin_arm64.tar.gz'
-      sha256 'b5be01f7e18bf1b81a291f1b1735340ef0cf2c09681844d96dabff8f8646d20c'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.7.0/tfstate-lookup_1.7.0_darwin_arm64.tar.gz'
+      sha256 '1faa01e048df39472c0c3d67d4b28c9f77e8a2efd3f7e12e45ae6a1fc716decb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.6.0/tfstate-lookup_1.6.0_darwin_amd64.tar.gz'
-      sha256 '73bf64156af87382b8c409202ed1b953ce064d6220c304e05fcaeb71a6313017'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.7.0/tfstate-lookup_1.7.0_darwin_amd64.tar.gz'
+      sha256 '93bd4326482e068eecfa9c1436b4d763ad02928ed7c8cf7f9e6d5ce4ebb92f78'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.6.0/tfstate-lookup_1.6.0_linux_arm64.tar.gz'
-      sha256 'f1142ab941d42c590bdcd544126a94667a2c39f2c6275973affea7a474fa7338'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.7.0/tfstate-lookup_1.7.0_linux_arm64.tar.gz'
+      sha256 'a0a260230122ba5b2ae73a5c3d925485a084e9b6d49405eea6e8a9784e2451aa'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.6.0/tfstate-lookup_1.6.0_linux_amd64.tar.gz'
-      sha256 '0d92f8d3dfdcf473d3d26d2f95078bc69f20b3b46bdf86d98200fe1205299bb4'
+      url 'https://github.com/fujiwara/tfstate-lookup/releases/download/v1.7.0/tfstate-lookup_1.7.0_linux_amd64.tar.gz'
+      sha256 'c8060a86d09e5d79219037fba0b7aaa940442355d8c48e725d8600d1375dd3f8'
     end
   end
 
