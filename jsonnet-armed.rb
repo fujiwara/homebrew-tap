e117f2b2180200392c6cfecb1876d9eea66cf167
fujiwara <fujiwara.shunichiro@gmail.com>
2025-09-16 09:12:42 +0000

    update to newer version

--- jsonnet-armed.rb
+++ jsonnet-armed.rb
@@ -1,27 +1,27 @@
 class JsonnetArmed < Formula
   desc 'A Jsonnet rendering tool with additional useful functions.'
-  version '0.0.5'
+  version '0.0.7'
   homepage 'https://github.com/fujiwara/jsonnet-armed'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.5/jsonnet-armed_0.0.5_darwin_arm64.tar.gz'
-      sha256 'e858c07fbd2bc9cf3130dd88b83e7b8ce8251fb4434dfc89af716191d6d66c9e'
+      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.7/jsonnet-armed_0.0.7_darwin_arm64.tar.gz'
+      sha256 '45c4ffe91a3149af289840ea73d0ce986db7c3cabde3372d42a6cf737ded9ccb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.5/jsonnet-armed_0.0.5_darwin_amd64.tar.gz'
-      sha256 '305d0be56fb10c7e13f73b249a20c7fbebd4e7a08c4af16362674010a27fce49'
+      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.7/jsonnet-armed_0.0.7_darwin_amd64.tar.gz'
+      sha256 '3dfe56a793763a3b47122bda8086a5a83920914def6a17bc4163cde48a239f70'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.5/jsonnet-armed_0.0.5_linux_arm64.tar.gz'
-      sha256 'e9c32e12644ce0a7dcf2ec89a616d25f16e418a66ab39cae2d093e70aad336f6'
+      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.7/jsonnet-armed_0.0.7_linux_arm64.tar.gz'
+      sha256 'f27bd9c4cb58e676639758ca5dab44b47c95c72000f6227bf251d894d2f810fe'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.5/jsonnet-armed_0.0.5_linux_amd64.tar.gz'
-      sha256 'cc92527361ca1783fbc8004448993983a8733ce762f3003ef8679a9ee9e257d7'
+      url 'https://github.com/fujiwara/jsonnet-armed/releases/download/v0.0.7/jsonnet-armed_0.0.7_linux_amd64.tar.gz'
+      sha256 '33959c9cc5c5d2a37bd5911775a080d019a54dd9392072006de3c704a688e40b'
     end
   end
 
