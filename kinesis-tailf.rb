b29d7647e23670574b6ddd6a2690cf394dbacdaa
fujiwara <fujiwara.shunichiro@gmail.com>
2025-11-30 13:15:42 +0000

    update to newer version

--- kinesis-tailf.rb
+++ kinesis-tailf.rb
@@ -1,27 +1,27 @@
 class KinesisTailf < Formula
   desc 'tail -f command for Amazon Kinesis Stream'
-  version '0.5.0'
+  version '0.5.1'
   homepage 'https://github.com/fujiwara/kinesis-tailf'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_darwin_arm64.tar.gz'
-      sha256 'b98ccea9d7c2a03a351577a8c94ed829d9e7323d9fb4c005268f67e408ed0717'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.1/kinesis-tailf_0.5.1_darwin_arm64.tar.gz'
+      sha256 '3238ee2e70e1e39df5671804e5d298a7b15b4c3ab563cd759b387102858fceac'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_darwin_amd64.tar.gz'
-      sha256 'bde1fa83bd5a3439c2aaa1e0e6f77bb44d6844b09fe32b30f91b505a92b436de'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.1/kinesis-tailf_0.5.1_darwin_amd64.tar.gz'
+      sha256 '9882f58632522d15fa2ead2970d67ba499b10f984936fa0df782e3cbfdfb9272'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_linux_arm64.tar.gz'
-      sha256 '96ecf927be0f784b83d51726cf0c9b3db363df5b62537b53d6cc9a5a76a9a48c'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.1/kinesis-tailf_0.5.1_linux_arm64.tar.gz'
+      sha256 '3aac1578a821e4bfe07368ea9b6dd774675c98ac26e703d767aae8c7ea33076b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.0/kinesis-tailf_0.5.0_linux_amd64.tar.gz'
-      sha256 '9c08ebe62f9a4893790140074dc8deac0391d1c935da6895657e0e3457523c3c'
+      url 'https://github.com/fujiwara/kinesis-tailf/releases/download/v0.5.1/kinesis-tailf_0.5.1_linux_amd64.tar.gz'
+      sha256 '1b1caea363b4272a0d652a49c3b89f6f93effa6986fd8ef9cf2592ac180e345e'
     end
   end
 
--- tracer.rb
+++ tracer.rb
@@ -1,27 +1,27 @@
 class Tracer < Formula
   desc 'ECS task event/log tracer CLI'
-  version '1.1.2'
+  version '1.1.3'
   homepage 'https://github.com/fujiwara/tracer'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.2/tracer_1.1.2_darwin_arm64.tar.gz'
-      sha256 '2ad7fa4a4d160137dbeb2e4d010feebdbc4db6d4056b5f7007435b7aa880c503'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.3/tracer_1.1.3_darwin_arm64.tar.gz'
+      sha256 '4c973d65d4e50e4c30e48a5e084939d27c1efc752f18c36094778695f32176d1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.2/tracer_1.1.2_darwin_amd64.tar.gz'
-      sha256 'b5973003ca7610c20458b6194ef3f6d39c081326a0a9c43125e30146c968ea5a'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.3/tracer_1.1.3_darwin_amd64.tar.gz'
+      sha256 '7cbb035cb2ccffb4c86987ef3606daf1f3f1d92e21b15018fc593b80aad79b9f'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.2/tracer_1.1.2_linux_arm64.tar.gz'
-      sha256 '20ee352c47a0cb699016cf98133751bf4785adbd2cb0890bb03a40793e819d41'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.3/tracer_1.1.3_linux_arm64.tar.gz'
+      sha256 '6b31a43c484e5fd440d6a17683e6a88ab4c35311f3a2f97c9af9c5413ff34a7e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.2/tracer_1.1.2_linux_amd64.tar.gz'
-      sha256 '9655512ca41d39208f0290a966359b62b710092c5b0a2a90b25a5886cafc1f8c'
+      url 'https://github.com/fujiwara/tracer/releases/download/v1.1.3/tracer_1.1.3_linux_amd64.tar.gz'
+      sha256 '35bd6b73ee4e5960274a40792ce701efe25641d9d4addb2a2d4c0332fbf0d8a4'
     end
   end
 
