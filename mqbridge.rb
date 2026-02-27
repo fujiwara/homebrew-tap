7e753fac23b7e137402f9e06f16f3f3e490aaf26
fujiwara <fujiwara.shunichiro@gmail.com>
2026-02-27 18:45:54 +0000

    update to newer version

--- mqbridge.rb
+++ mqbridge.rb
@@ -1,27 +1,27 @@
 class Mqbridge < Formula
   desc 'mqbridge is a replicator between RabbitMQ and Sakura SimpleMQ'
-  version '0.0.1'
+  version '0.0.2'
   homepage 'https://github.com/fujiwara/mqbridge'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.1/mqbridge_v0.0.1_darwin_arm64.tar.gz'
-      sha256 '53562df76206c268a5a0b5ed5d1357d244bf820af0f78c8fe6213a682f64f8ec'
+      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.2/mqbridge_v0.0.2_darwin_arm64.tar.gz'
+      sha256 'b082c5d6bfbaad908535c4b82dfbef17925862d64ba887fe91493ab715f79d43'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.1/mqbridge_v0.0.1_darwin_amd64.tar.gz'
-      sha256 '6cf08540a6d86bbe8d8f72dac27320af6c427487dbdae3dbd9093211aed84eec'
+      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.2/mqbridge_v0.0.2_darwin_amd64.tar.gz'
+      sha256 'ca7f8a0b34be57f8e500fffd81af4fffa84822687780af4e86323ae974913a3e'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.1/mqbridge_v0.0.1_linux_arm64.tar.gz'
-      sha256 'cbcbcdb5aa390bffedbc9d53fb1ad5db2a323f5d7057254a2c97fa224416e178'
+      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.2/mqbridge_v0.0.2_linux_arm64.tar.gz'
+      sha256 '5e2bfe1134d8c9925fc2d0a79aab9bfd8f18f5c8779691878ebb3a324a46a44f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.1/mqbridge_v0.0.1_linux_amd64.tar.gz'
-      sha256 'eedf658397e4d6c21e88ff142aacac115e937f0066f00ea4995c2d3601b33327'
+      url 'https://github.com/fujiwara/mqbridge/releases/download/v0.0.2/mqbridge_v0.0.2_linux_amd64.tar.gz'
+      sha256 '6b56bda68ae1d8ba07c1d9d07b2df8c6a262ebd1eeb0d03351ded4d050018768'
     end
   end
 
