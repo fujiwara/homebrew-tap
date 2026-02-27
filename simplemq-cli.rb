1a4d0773cc1c498c92949aaa9e625e0e5d352aa0
fujiwara <fujiwara.shunichiro@gmail.com>
2026-02-27 09:45:48 +0000

    update to newer version

--- simplemq-cli.rb
+++ simplemq-cli.rb
@@ -1,26 +1,26 @@
 class SimplemqCli < Formula
-  version '0.2.1'
+  version '0.2.2'
   homepage 'https://github.com/fujiwara/simplemq-cli'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.1/simplemq-cli_v0.2.1_darwin_arm64.tar.gz'
-      sha256 '344337a31b60531537fc48c6afdb1675afab87fd95b3a0fb42b68a39d2a28319'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.2/simplemq-cli_v0.2.2_darwin_arm64.tar.gz'
+      sha256 '2a30fef52428857cd3496a3e923f168aa3b582677f3706e602a0a7a22e310f31'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.1/simplemq-cli_v0.2.1_darwin_amd64.tar.gz'
-      sha256 '2d233bd2b67e72d2f7ec881b0dba2a773ee85daf36c5599233bdbdec7f11bd58'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.2/simplemq-cli_v0.2.2_darwin_amd64.tar.gz'
+      sha256 '15d0228445f221f7006da03c6c0702ebfbcb44f7da8d08d22dc6e4cea61be7d5'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.1/simplemq-cli_v0.2.1_linux_arm64.tar.gz'
-      sha256 'f19023e621355305585cad5ff60b7a2e2d35949ae12e480b45e06d5fd9888b04'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.2/simplemq-cli_v0.2.2_linux_arm64.tar.gz'
+      sha256 'fd7a8486ea8558288d6d213952356ab8a6382e77dee93142f21a6c021fc2d6b4'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.1/simplemq-cli_v0.2.1_linux_amd64.tar.gz'
-      sha256 'dc52d0f2d5c9f760baedbd2d0b14cf0970f283579d8c31baa637b4b9ebcc9a05'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.2.2/simplemq-cli_v0.2.2_linux_amd64.tar.gz'
+      sha256 '74cd344cf99fca05f90e736e28b6f981b28ce0d9d36ab8447ac1d7083ec5233c'
     end
   end
 
