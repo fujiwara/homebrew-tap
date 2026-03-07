e7dff9f8388bf09d28d5cc8b9b6fbbb98b897fb8
fujiwara <fujiwara.shunichiro@gmail.com>
2026-03-07 09:41:12 +0000

    update to newer version

--- simplemq-cli.rb
+++ simplemq-cli.rb
@@ -1,26 +1,26 @@
 class SimplemqCli < Formula
-  version '0.6.0'
+  version '0.7.0'
   homepage 'https://github.com/fujiwara/simplemq-cli'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.6.0/simplemq-localserver_0.6.0_darwin_arm64.tar.gz'
-      sha256 '6883d030f1797fdcfb542cba1031fc36e50f62dd17ac2424ab0fb386682fed7d'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-localserver_0.7.0_darwin_arm64.tar.gz'
+      sha256 '8f9a0b4091e972575a7fb47e89d8fba1e6117d090551957ada7bdffd508b68ee'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.6.0/simplemq-localserver_0.6.0_darwin_amd64.tar.gz'
-      sha256 '01849764cac41cf732a6d3b2f5148672ec7be91f9205bb20945498230a83ace6'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-localserver_0.7.0_darwin_amd64.tar.gz'
+      sha256 '500b1c0ccc0c8841163c010608ffa7109a1c8daedf1a1daa2ed3845f860ab974'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.6.0/simplemq-localserver_0.6.0_linux_arm64.tar.gz'
-      sha256 'd4f31a9ff6421882e813a85e69be04fdd00ac813a551a133ef1ad4534445ccf9'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-localserver_0.7.0_linux_arm64.tar.gz'
+      sha256 '3092660f23df4b4a619285a05d0aad0e9ac8ddd5f33c5d10d5520c22af63282c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.6.0/simplemq-localserver_0.6.0_linux_amd64.tar.gz'
-      sha256 'f800b3c0e419b7367987490da5b213ab50f47a500d6723016f6378c18a2d4786'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-localserver_0.7.0_linux_amd64.tar.gz'
+      sha256 '2f1a95240f715e31e002152ea8b51d0d262c9bde0616898ec33158a1fb6f7c39'
     end
   end
 
