61675192c74af82c3577974d709e45d016870218
fujiwara <fujiwara.shunichiro@gmail.com>
2026-03-09 05:38:06 +0000

    update to newer version

--- simplemq-cli.rb
+++ simplemq-cli.rb
@@ -1,27 +1,27 @@
 class SimplemqCli < Formula
-  version '0.7.0'
+  version '0.7.1'
   homepage 'https://github.com/fujiwara/simplemq-cli'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-cli_0.7.0_darwin_arm64.tar.gz'
-      sha256 'c0707e42104445eca38fd946d4e99a6133fc066a3f765ede91257057327bd609'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.1/simplemq-cli_0.7.1_darwin_arm64.tar.gz'
+      sha256 '1c6caec58636a0f435756c2918ab27895798dc3af9f7d2d90710e6c76577562c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-cli_0.7.0_darwin_amd64.tar.gz'
-      sha256 '198137f74e5ad132f6b8b2d2e322337fcbac5deb5894a132e0de3f31073cd2db'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.1/simplemq-cli_0.7.1_darwin_amd64.tar.gz'
+      sha256 '2853e6d32d67273ed0dca7a9958b1a19675eb4dddd949c4a37abafd8bf596f3d'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-cli_0.7.0_linux_arm64.tar.gz'
-      sha256 '61b51d2c37734ebec8feb2cb02b05759102e457e8ed58415ecd952ed8efeb77a'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.1/simplemq-cli_0.7.1_linux_arm64.tar.gz'
+      sha256 'd2c1039f9f0174c25da65068f7036db1e10bdc371dee266e6c9ffbc3a7f51d86'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.0/simplemq-cli_0.7.0_linux_amd64.tar.gz'
-      sha256 'bf696e6f716c4ecd96f8a4f04e64d1ac64b770303b34dbe8ae1e89572948f4ca'
+      url 'https://github.com/fujiwara/simplemq-cli/releases/download/v0.7.1/simplemq-cli_0.7.1_linux_amd64.tar.gz'
+      sha256 'dfe4e76e0639400711cd31542e60e956babf8a7af01654aa26c0445216940080'
     end
   end
 
