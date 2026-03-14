9aa5f00e2a912bbaab52b1efbc2b838660b440f8
fujiwara <fujiwara.shunichiro@gmail.com>
2026-03-14 06:51:29 +0000

    update to newer version

--- apprun-dedicated-cli.rb
+++ apprun-dedicated-cli.rb
@@ -1,28 +1,28 @@
 class ApprunDedicatedCli < Formula
   desc 'A deploy tool for Sakura Cloud AppRun Dedicated(共有型).'
-  version '0.0.1'
+  version '0.1.0'
   homepage 'https://github.com/fujiwara/apprun-dedicated-cli'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.0.1/apprun-dedicated-cli_v0.0.1_darwin_arm64.tar.gz'
-      sha256 '853f35b4aeeb4d059f94df78b3303e7182d259f419dae859af1624ffcd9548a8'
+      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.1.0/apprun-dedicated-cli_v0.1.0_darwin_arm64.tar.gz'
+      sha256 '9dc56cd801e5dd8f35101210b351dbc2b1198e09d57e825f2b83a6a9e09c9cc6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.0.1/apprun-dedicated-cli_v0.0.1_darwin_amd64.tar.gz'
-      sha256 'e1486adf88d0c101d276f480b9e3db73ace9cbfbb14bc2083f9e11dc5480062e'
+      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.1.0/apprun-dedicated-cli_v0.1.0_darwin_amd64.tar.gz'
+      sha256 'a074a11cdf20a90c7183beff074f2e95ed2877f6141e37a25106426a72fbd5f1'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.0.1/apprun-dedicated-cli_v0.0.1_linux_arm64.tar.gz'
-      sha256 'cf80d97df3bb0171b663dbe773d17a1084a2580eee709073c005fb29ee19867a'
+      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.1.0/apprun-dedicated-cli_v0.1.0_linux_arm64.tar.gz'
+      sha256 '7e60eb697525e9e429780bf2dfb29030e16e352ab233749d076aa8484c8a2d99'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.0.1/apprun-dedicated-cli_v0.0.1_linux_amd64.tar.gz'
-      sha256 '672a100e1ab5acc488d474f9ed782b932c6895a39db95fc3465abf464dedc25d'
+      url 'https://github.com/fujiwara/apprun-dedicated-cli/releases/download/v0.1.0/apprun-dedicated-cli_v0.1.0_linux_amd64.tar.gz'
+      sha256 'f250303af270dbb173daa270e8eb119adda0dfa16360c1464d6c364d999ec349'
     end
   end
 
