e5b29b2e1d73e187ed72165904cf6163142fbe52
fujiwara <fujiwara.shunichiro@gmail.com>
2026-03-19 09:46:16 +0000

    update to newer version

--- sops-sakura-kms.rb
+++ sops-sakura-kms.rb
@@ -1,26 +1,26 @@
 class SopsSakuraKms < Formula
-  version '0.4.0'
+  version '0.5.0'
   homepage 'https://github.com/fujiwara/sops-sakura-kms'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.4.0/sops-sakura-kms_v0.4.0_darwin_arm64.tar.gz'
-      sha256 'c5b738e36fb88a80129ffd6acd3c9fc94ed9a625ff3f0996a1b07c9eae05a7a5'
+      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.5.0/sops-sakura-kms_v0.5.0_darwin_arm64.tar.gz'
+      sha256 'd56fc986997a581663311feea565ec33a69731dd067f32a92afed834d7e35b26'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.4.0/sops-sakura-kms_v0.4.0_darwin_amd64.tar.gz'
-      sha256 '95ca17c04e821bf1aca9cbe98616d56115f6efc8a352bd43189789ab53a84ddd'
+      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.5.0/sops-sakura-kms_v0.5.0_darwin_amd64.tar.gz'
+      sha256 'b9d1d01853ac869b7c97a13fe92c5627a552a280b832d677a5f1405badca7ac1'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.4.0/sops-sakura-kms_v0.4.0_linux_arm64.tar.gz'
-      sha256 'f691ffad37aa0a1c322bd2a1d0e561b97c605c066e8ad35c106855ab7bae5444'
+      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.5.0/sops-sakura-kms_v0.5.0_linux_arm64.tar.gz'
+      sha256 '93c09343288d865ed5fd1463d1a972db849348a1ba07a95e52b1014ddba844c7'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.4.0/sops-sakura-kms_v0.4.0_linux_amd64.tar.gz'
-      sha256 '51dcd094dc13080f86e45d35e8c2f5d8f589531f35f2ea1de2f7465f879da652'
+      url 'https://github.com/fujiwara/sops-sakura-kms/releases/download/v0.5.0/sops-sakura-kms_v0.5.0_linux_amd64.tar.gz'
+      sha256 'f620fc83dcddb5e79662438d3fe054d7680e28efaebfa7f73767c7f9b5b84b86'
     end
   end
 
