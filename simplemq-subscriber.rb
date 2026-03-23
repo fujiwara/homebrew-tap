ed568d70b5a0690ebd2e6d0aa4187692d4495c09
fujiwara <fujiwara.shunichiro@gmail.com>
2026-03-23 09:49:02 +0000

    update to newer version

--- simplemq-subscriber.rb
+++ simplemq-subscriber.rb
@@ -1,28 +1,28 @@
 class SimplemqSubscriber < Formula
   desc 'simplemq-subscriber is a subscriber for Sakura SimpleMQ'
-  version '0.1.0'
+  version '0.2.0'
   homepage 'https://github.com/fujiwara/simplemq-subscriber'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.1.0/simplemq-subscriber_v0.1.0_darwin_arm64.tar.gz'
-      sha256 'c1b6761b366e209a4995813caffe00b6aa8a9edee2f8f360ead5d1a9d7e2e2ed'
+      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.2.0/simplemq-subscriber_v0.2.0_darwin_arm64.tar.gz'
+      sha256 '7e1f61bc633a4108c05812b6b703f74731bebc8d7b642eabd7df0a951e549333'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.1.0/simplemq-subscriber_v0.1.0_darwin_amd64.tar.gz'
-      sha256 '6c3a76de6e6452c12f493d5774dac47f81160af7815ae70a6afb5ced49acee28'
+      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.2.0/simplemq-subscriber_v0.2.0_darwin_amd64.tar.gz'
+      sha256 'e88eeb409d596791a366f6f8709daa3437ab6e4a18e3cd25a1f68973d3488c98'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.1.0/simplemq-subscriber_v0.1.0_linux_arm64.tar.gz'
-      sha256 '67417560cde32ee9cafd4bdde09c8e1acf3eccb401b5923b04bffa8de4dee2a9'
+      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.2.0/simplemq-subscriber_v0.2.0_linux_arm64.tar.gz'
+      sha256 'f957325d7bba09d527f0ed37c3211dea49761ad20693857c19c8b1553b3e8810'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.1.0/simplemq-subscriber_v0.1.0_linux_amd64.tar.gz'
-      sha256 '78b8b5362e5b9aa5a7495e9afdd24dbc98b224728862fdcdfb200c68c36a451d'
+      url 'https://github.com/fujiwara/simplemq-subscriber/releases/download/v0.2.0/simplemq-subscriber_v0.2.0_linux_amd64.tar.gz'
+      sha256 '2a5663b4d2402c945f20fc216f0edb200a5495c220d18e83f00fe9f695bfec12'
     end
   end
 
