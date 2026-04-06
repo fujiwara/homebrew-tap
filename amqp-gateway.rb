931e4f6f72419fb4bce78871abad915ccbd8438c
fujiwara <fujiwara.shunichiro@gmail.com>
2026-04-06 09:52:47 +0000

    update to newer version

--- amqp-gateway.rb
+++ amqp-gateway.rb
@@ -1,28 +1,28 @@
 class AmqpGateway < Formula
   desc 'Publish messages to AMQP broker (RabbitMQ) via HTTP.'
-  version '0.0.2'
+  version '0.0.3'
   homepage 'https://github.com/fujiwara/amqp-gateway'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.2/amqp-gateway_v0.0.2_darwin_arm64.tar.gz'
-      sha256 'b3555c3e2aa3b12948d2dd116510e110a954a9016ac23cc0d6e2fe1b918da79a'
+      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.3/amqp-gateway_v0.0.3_darwin_arm64.tar.gz'
+      sha256 '614325c13fdfeca2200da06574ed4d9846f953c2082c16e3c90bbcd787184b7b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.2/amqp-gateway_v0.0.2_darwin_amd64.tar.gz'
-      sha256 'e9a40a9359794f04c994f48d0383090a0633dcdf298f6a0ca7dfc760e2dffb99'
+      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.3/amqp-gateway_v0.0.3_darwin_amd64.tar.gz'
+      sha256 'b6e81ba132b75c50b415fc24db9c8c5e29b38ed891efddda5eb161607087c60a'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.2/amqp-gateway_v0.0.2_linux_arm64.tar.gz'
-      sha256 'bd343f7f2cfa569ff3da0ebaf22c681076fdb1ad5757bc21e791d8428d8dfae3'
+      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.3/amqp-gateway_v0.0.3_linux_arm64.tar.gz'
+      sha256 'd01b4f4bffe395fe01a01a24326c9e7f90fe6507217b7297cd5c3fe6e5e2e60b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.2/amqp-gateway_v0.0.2_linux_amd64.tar.gz'
-      sha256 '57b74378eeddba3260b49ef52a986a5d381d459a20e0ccb4b05cb69e12b64876'
+      url 'https://github.com/fujiwara/amqp-gateway/releases/download/v0.0.3/amqp-gateway_v0.0.3_linux_amd64.tar.gz'
+      sha256 'f9489e07bd724c2a7c901bb6713b7c2b1f8394e550cbb3e09cfc87469f4085f2'
     end
   end
 
