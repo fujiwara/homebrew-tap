7d4b4075c929195f080aaa05079bd5e88973c54b
fujiwara <fujiwara.shunichiro@gmail.com>
2026-04-27 10:16:10 +0000

    update to newer version

--- mqsubscriber.rb
+++ mqsubscriber.rb
@@ -1,28 +1,28 @@
 class Mqsubscriber < Formula
   desc 'A subscriber for Sakura SimpleMQ and RabbitMQ.'
-  version '0.7.5'
+  version '0.7.6'
   homepage 'https://github.com/fujiwara/mqsubscriber'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.5/mqsubscriber_v0.7.5_darwin_arm64.tar.gz'
-      sha256 '7989d716e9eec0677abeacec6579b48eef3c5c8f410e793cc6c61ce5f1a8d893'
+      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.6/mqsubscriber_v0.7.6_darwin_arm64.tar.gz'
+      sha256 'fe99bd7572181b0585d74bdaf7d18234de4b578007b3ee7cd3a35e955ab45784'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.5/mqsubscriber_v0.7.5_darwin_amd64.tar.gz'
-      sha256 'f183d669f439baf065b274d3b3b366d035a43458ce8816441fbec5d245d45af9'
+      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.6/mqsubscriber_v0.7.6_darwin_amd64.tar.gz'
+      sha256 'b1f0943dfa493e9840e48c0b334766942d13515f6ab5a7b43f3f4864a39d5fdf'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.5/mqsubscriber_v0.7.5_linux_arm64.tar.gz'
-      sha256 '982a636d966bffd98f84e4a8f00d645f4c093a6a38628b9a25c2389a07c8871b'
+      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.6/mqsubscriber_v0.7.6_linux_arm64.tar.gz'
+      sha256 '9a88626e40c0d8894e0e6159b06d0e517ba018ec8a5937bc4ecb52e046064a7d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.5/mqsubscriber_v0.7.5_linux_amd64.tar.gz'
-      sha256 '3ad2449bfc935eda992c9ac94d09a1924089ff094111fb57276f80e4135b5d16'
+      url 'https://github.com/fujiwara/mqsubscriber/releases/download/v0.7.6/mqsubscriber_v0.7.6_linux_amd64.tar.gz'
+      sha256 '180e78016a14c0b96b8248751bc5149cce78d918689ba935e70038f153b57623'
     end
   end
 
