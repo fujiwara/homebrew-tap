5c23e9215d4fc0e0fe7e0a0db06709b13df0ccdd
fujiwara <fujiwara.shunichiro@gmail.com>
2026-05-01 18:50:16 +0000

    update to newer version

--- sakura-secrets-cli.rb
+++ sakura-secrets-cli.rb
@@ -1,26 +1,26 @@
 class SakuraSecretsCli < Formula
-  version '0.3.0'
+  version '0.3.1'
   homepage 'https://github.com/fujiwara/sakura-secrets-cli'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.0/sakura-secrets-cli_v0.3.0_darwin_arm64.tar.gz'
-      sha256 '87d0902aa9e235af1ce874714efff4555ea278e61788e1fb0c32eb7e09541714'
+      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.1/sakura-secrets-cli_v0.3.1_darwin_arm64.tar.gz'
+      sha256 'cff90787240e739f5c337c08b86eef3ae947921aeeddb26755bbdd2e07467646'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.0/sakura-secrets-cli_v0.3.0_darwin_amd64.tar.gz'
-      sha256 'c8666f3109bc290695da92a9a1250073d3ad4a61ab3db88ffa019ad7825daa64'
+      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.1/sakura-secrets-cli_v0.3.1_darwin_amd64.tar.gz'
+      sha256 '507b5decc4a1f45c4c460c22ed40b6b55fe3b04195d7faf28be1afa1eac48fe8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.0/sakura-secrets-cli_v0.3.0_linux_arm64.tar.gz'
-      sha256 'a9ad3e9d23fbb3d542926aa2ee119689819f51cfc9844a76a4215c894cd5d04b'
+      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.1/sakura-secrets-cli_v0.3.1_linux_arm64.tar.gz'
+      sha256 '8ed10dbadab81c7dc50ee6898b3ac4c4df807f9600fd06bc7170a43df4001e49'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.0/sakura-secrets-cli_v0.3.0_linux_amd64.tar.gz'
-      sha256 '9e191029d75918c3db27efc23da59f03a8a180d28e60cfcb0533a8207d0d6594'
+      url 'https://github.com/fujiwara/sakura-secrets-cli/releases/download/v0.3.1/sakura-secrets-cli_v0.3.1_linux_amd64.tar.gz'
+      sha256 '13d90ed632db79ab84cbb9070673b0ddfb395a85a3fa53d919f8a8f0d941e77a'
     end
   end
 
