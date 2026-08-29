eaff3378041f5dc738407212b892669bf65ce9d3
fujiwara <fujiwara.shunichiro@gmail.com>
2026-08-29 05:06:32 +0000

    update to newer version

--- awslim.rb
+++ awslim.rb
@@ -1,27 +1,27 @@
 class Awslim < Formula
   desc 'A simplified alternative to the AWS CLI for limited use cases.'
-  version '0.7.0'
+  version '0.7.1'
   homepage 'https://github.com/fujiwara/awslim'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.0/awslim_0.7.0_darwin_arm64.tar.gz'
-      sha256 'f884115e50b24165a715f2319c8cc13db4ae64f64d01562f1abc3ea3ca41624d'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.1/awslim_0.7.1_darwin_arm64.tar.gz'
+      sha256 '848d2b0915e6bd83ee68a51d21922d8d14c4412813dc10c1c9bea4c5a58f9744'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.0/awslim_0.7.0_darwin_amd64.tar.gz'
-      sha256 'b23827eeb15048623e24811f13da03741cce1c6bc6dfd8d7c2b0e3b1338c4bac'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.1/awslim_0.7.1_darwin_amd64.tar.gz'
+      sha256 '955d5ebade42aa8bb052f181f58e78d6ef0826dc9aec67e6fc42d0369753e51f'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.0/awslim_0.7.0_linux_arm64.tar.gz'
-      sha256 '8407e8975f387791202600934f4ff4d77533595a1f0421dfb25d301cb9be9234'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.1/awslim_0.7.1_linux_arm64.tar.gz'
+      sha256 'faf79a29b49eb2333711bae2b86162a5d6dae56069a4c38804704c9a4650bd91'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.0/awslim_0.7.0_linux_amd64.tar.gz'
-      sha256 'e02364300c1e25ef72d1d08a53b39a9b9cfad79a00421e0120a89254ccb11a93'
+      url 'https://github.com/fujiwara/awslim/releases/download/v0.7.1/awslim_0.7.1_linux_amd64.tar.gz'
+      sha256 '8495751f3c3760b7798560653b81fe71fdad40a394c670fb86261b21e29da31a'
     end
   end
 
